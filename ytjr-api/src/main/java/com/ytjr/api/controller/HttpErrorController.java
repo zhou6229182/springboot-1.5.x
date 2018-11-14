4c6c52d0157b978686820c924dff8905ce394d01
cwgsityw-yutong.com <cwgsityw@ZZYT.com>
2018-11-14 17:20:27 +0800

    ResponseEntity

--- ytjr-api/src/main/java/com/ytjr/api/controller/HttpErrorController.java
+++ ytjr-api/src/main/java/com/ytjr/api/controller/HttpErrorController.java
@@ -3,6 +3,7 @@ package com.ytjr.api.controller;
 import com.ytjr.api.utils.R;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 public class HttpErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public R handleError(HttpServletRequest request) {
+    public ResponseEntity handleError(HttpServletRequest request) {
         Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
         if (statusCode == null) {
-            return R.error(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+            return ResponseEntity.ok(R.error(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()));
         } else {
             try {
-                return R.error(String.valueOf(statusCode), HttpStatus.valueOf(statusCode).getReasonPhrase());
+                return ResponseEntity.ok(R.error(String.valueOf(statusCode), HttpStatus.valueOf(statusCode).getReasonPhrase()));
             } catch (Exception e) {
-                return R.error(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+                return ResponseEntity.ok(R.error(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()));
             }
         }
     }
